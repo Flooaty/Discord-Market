e2ad3697b23a29115401eb71261550b90f0397b4
Flooaty <Floooaty@gmail.com>
2026-01-25 23:46:49 +0100

    Initial Commit

---
+++ src/main/java/plugin/discordMarket/DiscordMarket.java
@@ -0,0 +1,17 @@
+package plugin.discordMarket;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DiscordMarket extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
